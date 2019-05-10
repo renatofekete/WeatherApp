f51b8c6cc4fb39ef42e1d83e00cf55888272877a
renatofekete <renatofekete.91@gmail.com>
2019-05-10 19:51:17 +0200

    disabled

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,30 @@ window.onload = () => {
   let lat
   let long
   const key = '225fbbbcb8501c0acfbea4a81315fe4e'
-  let app = document.getElementById('app')
+  const app = document.getElementById('app')
+  // TEMPLATES
+  const displayWeather = (weather, time) => {
+    app.innerHTML = `
+      <div class="title">
+      <h1>${weather.name}, ${weather.sys.country}</h1>
+      <p class="date">${time.toLocaleString('hr-HR', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' })}:00h</p>
+      </div>
+      <div class="main">
+      <h2> ${Math.round(weather.main.temp - 273.15)}° </h2>
+      <div class="display">
+      <div class="img"><img src ='https://openweathermap.org/img/w/${weather.weather[0].icon}.png' alt="${weather.weather[0].main}" srcset=""></div>
+      <p>${weather.weather[0].description}</p>
+      </div>
+      </div>
+      `
+  }
+  const noGeolocation = () => {
+    app.innerHTML = `
+      <div class="title">
+      <h1>For this app to work, please allow location</h1>
+      </div>
+      `
+  }
   // INITIAL CALL
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -17,24 +40,6 @@ window.onload = () => {
           let time = new Date(data.dt * 1000)
           displayWeather(data, time)
         })
-    })
-  } else {
-
-  }
-  // INITIAL CALL TEMPLATE
-  let displayWeather = (weather, time) => {
-    app.innerHTML = `
-    <div class="title">
-    <h1>${weather.name}, ${weather.sys.country}</h1>
-    <p class="date">${time.toLocaleString('hr-HR', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' })}:00h</p>
-    </div>
-    <div class="main">
-    <h2> ${Math.round(weather.main.temp - 273.15)}° </h2>
-    <div class="display">
-    <div class="img"><img src ='https://openweathermap.org/img/w/${weather.weather[0].icon}.png' alt="${weather.weather[0].main}" srcset=""></div>
-    <p>${weather.weather[0].description}</p>
-    </div>
-    </div>
-    `
+    }, noGeolocation)
   }
 }
