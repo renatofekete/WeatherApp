6b8a6d9283b46eac9e9fef69842915605434518a
renatofekete <renatofekete.91@gmail.com>
2019-05-13 19:52:49 +0200

    five day call

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ window.onload = () => {
   let long
   const key = 'd4bf21aa78d1417f919046208837be54'
   const app = document.getElementById('app')
+  let isSingle = true
+  const showOne = document.querySelector('.show-one')
+  const showFive = document.querySelector('.show-five')
   // TEMPLATES
   const displayWeather = (weather) => {
     let time = new Date(weather.data[0].ts * 1000)
@@ -20,6 +23,26 @@ window.onload = () => {
       </div>
       </div>
       `
+  }
+  const displayFiveDayWeather = (weather) => {
+    app.innerHTML = `
+    <div class="title">
+    <h1>${weather.city_name}, ${weather.country_code}</h1>
+    <h3>5 days weather</h3>
+    </div>
+    <div class="main"><div class="days"></div></div>
+    `
+    const main = document.querySelector('.days')
+    const days = weather.data.map(day => {
+      let time = new Date(day.ts * 1000)
+      main.innerHTML += `
+      <div class="card">
+        <h3>${time.toLocaleString('hr-HR', { month: 'numeric', day: 'numeric' })}</h3>
+        <div class="img"><img src='assets/icons/${day.weather.icon}.png'></div>
+        <h4>${Math.ceil(day.min_temp)}° / ${Math.ceil(day.max_temp)}°</h4> 
+      </div>
+      `
+    })
   }
   const noGeolocation = () => {
     app.innerHTML = `
@@ -39,21 +62,42 @@ window.onload = () => {
       })
   }
 
-  /* const fiveDayCall = (lat, long) => {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?APPID=${key}&lat=${lat}&lon=${long}&cnt=5`)
+  const fiveDayCall = (lat, long) => {
+    fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${long}&key=${key}&days=5`)
       .then(response => {
         return response.json()
       })
       .then(data => {
-        console.log(data)
+        displayFiveDayWeather(data)
       })
-  } */
+  }
   // INITIAL CALL
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       lat = position.coords.latitude
       long = position.coords.longitude
       singleCall(lat, long)
+
+      showOne.addEventListener('click', () => {
+        if (!isSingle) {
+          showOne.classList.remove('active')
+          showFive.classList.add('active')
+          singleCall(lat, long)
+          isSingle = !isSingle
+        } else {
+
+        }
+      })
+      showFive.addEventListener('click', () => {
+        if (isSingle) {
+          showFive.classList.remove('active')
+          showOne.classList.add('active')
+          fiveDayCall(lat, long)
+          isSingle = !isSingle
+        } else {
+
+        }
+      })
     }, noGeolocation)
   }
 }
